dbb6a1e8165586c7ce42320153ffa299bb45a81a
Maas,Midas M <midas.maas@student.fontys.nl>
2020-12-16 09:10:22 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: "#000000",
+        accent: "#E44200"
+      }
+    },
+    fontFamily: {
+      regular: ['League Gothic Regular'],
+      condensed: ['League Gothic Condensed']
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
